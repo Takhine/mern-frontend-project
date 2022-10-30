579db64dd92cbc80820edffcc40ac85a949b1ab2
Aniketh Nair <anikethnair.developer@gmail.com>
2022-10-30 05:49:35 +0530

    Drawer Backdrop and slide animation

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+import "./SideDrawer.css";
 
-export default function SideDrawer() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function SideDrawer({ onClick, show, children }) {
+  const content = (
+    <CSSTransition
+      in={show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={onClick}>{children}</aside>
+    </CSSTransition>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 }
